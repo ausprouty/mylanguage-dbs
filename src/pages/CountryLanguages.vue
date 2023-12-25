2b78f5bd725ca2162fc0452908a18828a1cffced
ausprouty <bob.prouty@powertochange.org.au>
2023-12-26 09:04:18 +1100

    working toward maps

--- src/pages/CountryLanguages.vue
+++ src/pages/CountryLanguages.vue
@@ -1,47 +1,31 @@
 <template>
-  <div>
-    <q-layout view="hHh lpR fFf">
-      <!-- Left Side -->
-      <q-layout-section>
-        <q-page-container>
-          <q-col cols="6">
-            <div style="background-color: #eee; height: 100vh">
-              <h4>Languages in {{ this.$route.params.countryName }}</h4>
-              <p>Which language do you speak?</p>
-              <ul v-for="language in languages" :key="language.languageCodeHL">
-                <li @click="handleLanguageClick(language.languageCodeJF)">
-                  {{ language.languageNameEnglish }}
-                </li>
-              </ul>
-              <iframe   src="https://api.arclight.org/videoPlayerUrl?refId=1_496-jf6103-0-0&playerStyle=default&autoplay=true"></iframe>
-            </div>
-          </q-col>
-        </q-page-container>
-      </q-layout-section>
-
-      <!-- Right Side -->
-      <q-layout-section>
-        <q-page-container>
-          <q-col cols="6">
-            <div style="background-color: #ccc; height: 100vh">
-              <div v-html="this.iframe"></div>
-              <!-- Loop through each card in the cards array -->
-              <div
-                v-for="language in languages"
-                :key="language.languageCodeHL"
-                class="col-6"
-              >
-                <q-card class="full-width shadow-2">
-                    <div>
-                      {{ language.languageNameEnglish }}
-                    </div>
-                </q-card>
-              </div>
-            </div>
-          </q-col>
-        </q-page-container>
-      </q-layout-section>
-    </q-layout>
+  <div class="row items-start">
+    <div  class="col-12">
+      <h4>Languages in {{ this.$route.params.countryName }}</h4>
+      <p>Which language do you speak?</p>
+    </div>
+    <div class="col-6">
+      <!-- Loop through each card in the cards array -->
+      <div
+        v-for="language in languages"
+        :key="language.languageCodeHL"
+        class="col-6"
+      >
+        <q-card
+          @click="handleLanguageClick(language)"
+          class="full-width shadow-2 custom-card"
+          :class="{'selected': this.selectedCard === language.languageCodeHL}"
+          style="padding: 10px"
+        >
+          <div class="text-h6">
+            {{ language.languageNameEnglish }}
+          </div>
+        </q-card>
+      </div>
+    </div>
+    <div class="col-6 parent-container">
+      <div class="video top-div" v-html="this.iframe"></div>
+    </div>
   </div>
 </template>
 
@@ -53,18 +37,21 @@ export default {
   data() {
     return {
       languages: [],
-      selectedOption: "",
+      selectedCard: null,
+      languageCodeHL: null,
       iframe: "",
       iframeStart:
         '<iframe   src="https://api.arclight.org/videoPlayerUrl?refId=1_',
-      iframeEnd: '-jf6103-0-0&playerStyle=default&autoplay=true"></iframe>',
+      iframeEnd: '-jf6111-0-0&start=22&end=47&playerStyle=default"></iframe>',
       iframeLanguageCodeJF: "",
     };
   },
   methods: {
-    handleLanguageClick(languageCodeJF) {
-      if (languageCodeJF) {
-        this.iframe = this.iframeStart + languageCodeJF + this.iframeEnd;
+    handleLanguageClick(language) {
+      this.selectedCard= language.languageCodeHL
+      console.log (this.selectedCard)
+      if (language.languageCodeJF) {
+        this.iframe = this.iframeStart + language.languageCodeJF + this.iframeEnd;
         console.log(this.iframe);
       } else {
         this.iframe = "No Audio";
@@ -81,3 +68,25 @@ export default {
   },
 };
 </script>
+<style scoped>
+.custom-card:hover {
+  background-color: #ff0; /* Change to your desired color */
+  transition: background-color 0.3s; /* Optional: adds a smooth transition effect */
+}
+.selected {
+  background-color: #0F0;  /* Add your desired highlight color */
+  /* Add any other styling for the selected card */
+}
+.video {
+  margin: auto;
+}
+.parent-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* this centers the child elements horizontally */
+}
+.top-div {
+  align-self: flex-start; /* aligns the div to the top of the container */
+}
+
+</style>
